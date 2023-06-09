f0fe05d10ae047cb6c5eb6409908b54ec8af37a9
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-06-09 10:18:58 +0600

    sign up page added

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,18 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Routes';
 import AuthProvider from './Provider/AuthProvider';
+import { HelmetProvider } from 'react-helmet-async';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <div className="max-w-screen-lg mx-auto">
-        <RouterProvider router={router} />
-      </div>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <div className="max-w-screen-lg mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
