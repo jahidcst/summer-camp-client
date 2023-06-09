afca5319ea0c5eb31c644814c0cd1e1ac37dd9d9
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-06-09 18:20:35 +0600

    errorpage

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,11 +5,13 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import Error from "../Shared/Error";
 
  export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main> ,
+      errorElement: <Error></Error>,
       children: [
         {
           path: "/",
