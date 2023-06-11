72f696c495f3052226bfa7d01012e024e5785839
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-06-11 13:34:47 +0600

    pic added

--- src/pages/Home/Extra/Extra.jsx
+++ src/pages/Home/Extra/Extra.jsx
@@ -1,4 +1,4 @@
-
+import { FaCheck} from 'react-icons/fa';
 const Extra = () => {
     return (
         <div className="my-12">
@@ -15,9 +15,35 @@ const Extra = () => {
                     <div className="card-body text-center text-black mt-5">
                         <h2 className=" text-2xl font-serif ">Standard</h2>
                         <div className="text-start pl-16 pb-3 text-lg text-gray-500">
-                             <p>Pay as you go</p>
-                            <p>Perfect for non-residence</p></div>
+                             <p className='flex items-center gap-2'> <FaCheck></FaCheck> Pay as you go</p>
+                            <p className='flex items-center gap-2'> <FaCheck></FaCheck>Perfect for non-residence</p></div>
                         <p className="text-lg text-gray-500"><span className="font-serif text-5xl font-semibold text-purple-400">$30</span> /per month</p>
+                        <div className="card-actions justify-center pt-4">
+                            <button className="btn btn-info rounded-br-3xl rounded-tl-3xl">Enroll  Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card card-compact w-96 bg-teal-100 shadow-xl">
+                    <figure><img className="" src="https://img.freepik.com/free-photo/health-beautiful-female-body-peace_1139-721.jpg?w=900&t=st=1686468670~exp=1686469270~hmac=7f246dabaec4940b2caaa54bf0a5f7b0f9ed641f0a5ce7e2140aca978041a904" alt="Shoes" /></figure>
+                    <div className="card-body text-center text-black mt-5">
+                        <h2 className=" text-2xl font-serif ">Professional</h2>
+                        <div className="text-start pl-16 pb-3 text-lg text-gray-500">
+                             <p className='flex items-center gap-2'> <FaCheck></FaCheck> Short-term comitment</p>
+                            <p className='flex items-center gap-2'> <FaCheck></FaCheck>Online training for all</p></div>
+                        <p className="text-lg text-gray-500"><span className="font-serif text-5xl font-semibold text-purple-400">$120</span> /per month</p>
+                        <div className="card-actions justify-center pt-4">
+                            <button className="btn btn-info rounded-br-3xl rounded-tl-3xl">Enroll  Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card card-compact w-96 bg-teal-100 shadow-xl">
+                    <figure><img className="" src="https://img.freepik.com/free-photo/young-blonde-woman-sportswear-is-meditating-yoga-mat-with-closed-eyes-recording_1268-17254.jpg?w=900&t=st=1686468812~exp=1686469412~hmac=ca39e61ebdaea307f04365457b89907b93e9d390bcfe82829164c35c01f8f6a4" alt="Shoes" /></figure>
+                    <div className="card-body text-center text-black mt-5">
+                        <h2 className=" text-2xl font-serif ">Private</h2>
+                        <div className="text-start pl-16 pb-3 text-lg text-gray-500">
+                             <p className='flex items-center gap-2'> <FaCheck></FaCheck> Long-term comitment</p>
+                            <p className='flex items-center gap-2'> <FaCheck></FaCheck>24/7 Available</p></div>
+                        <p className="text-lg text-gray-500"><span className="font-serif text-5xl font-semibold text-purple-400">$200</span> /per month</p>
                         <div className="card-actions justify-center pt-4">
                             <button className="btn btn-info rounded-br-3xl rounded-tl-3xl">Enroll  Now</button>
                         </div>
